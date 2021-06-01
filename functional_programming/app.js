d236467c5ed7562c0a846bfd37988cbcbaef4878
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-31 19:45:20 -0600

    add arrow function exception

--- functional_programming/app.js
+++ functional_programming/app.js
@@ -1,3 +1,4 @@
+//setting mapForEach function for future use
 function mapForEach(arr, fn) {
   var newArr = [];
   for (var i = 0; i < arr.length; i++) {
@@ -10,6 +11,7 @@ function mapForEach(arr, fn) {
 var arr1 = [1, 2, 3];
 console.log(arr1);
 
+//seeing mapForEach in action
 var arr2 = mapForEach(arr1, function (item) {
   return item * 2;
 });
@@ -20,12 +22,14 @@ var arr3 = mapForEach(arr1, function (item) {
 });
 console.log(arr3);
 
+//simplifying comparison above
 var checkPastLimit = function (limiter, item) {
   return item > limiter;
 };
 var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1)); //the one below is a copy
 console.log(arr4);
 
+//simplifying comparison above even more
 var checkPastLimitSimplified = function (limiter) {
   return function (limiter, item) {
     return item > limiter;
